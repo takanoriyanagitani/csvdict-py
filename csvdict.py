4a5050d834173eff5699e9e9591d54e05e2f182e
yanagitani <takanori.yanagitani@gmail.com>
2020-05-15 15:56:38 +0900

    nested dict -> str

--- csvdict.py
+++ csvdict.py
@@ -19,5 +19,5 @@ def ds2str(ds=None, s1=":", s2=";", d2k=dict2keys): return dict2str(ds,s1,s2, d2
 def dict2str2(d=dict(), s1="=|", s2=":;", d2k=dict2keys):
   keys = d2k(d)
   pairs = map(lambda k: (k, ds2str(d[k], s2[0], s2[1], d2k)), keys)
-  strs  = map(lambda p: s1[0].join(p), pairs)
+  strs  = map(lambda p: s1[0].join(map(str, p)), pairs)
   return s1[1].join(strs)
--- csvdict_test.py
+++ csvdict_test.py
@@ -19,6 +19,27 @@ class ds2str(unittest.TestCase):
   def test_tower(self): self.assertEqual("tst:634;tt:333", csvdict.ds2str(dict(tst=634, tt=333)))
   def test_mount(self): self.assertEqual("fuji=3776|takao=599", csvdict.ds2str(dict(fuji=3776,takao=599),"=","|"))
 
+class dict2str2(unittest.TestCase):
+  def test_empty(self): self.assertEqual("", csvdict.dict2str2(dict()))
+  def test_phone(self): self.assertEqual(
+    "cam1=name:front;res:2k|cam2=name:back;res:4k|cameras=2|type=phone",
+    csvdict.dict2str2(dict(
+      type="phone",
+      cameras=2,
+      cam1=dict(name="front", res="2k"),
+      cam2=dict(name="back",  res="4k"),
+    ))
+  )
+  def test_drive(self): self.assertEqual(
+    "count=3|d1=read:4GB/s;rotate:False;size:2TB;write:2GB/s|d2=read:2GB/s;rotate:False;size:1TB;write:1GB/s|raid=False",
+    csvdict.dict2str2(dict(
+      raid=False,
+      count=3,
+      d1=dict(size="2TB", rotate=False, write="2GB/s", read="4GB/s"),
+      d2=dict(size="1TB", rotate=False, write="1GB/s", read="2GB/s"),
+    ))
+  )
+
 def main(): return unittest.main()
 
 def try_exec(): return "__main__" == __name__ and main()
