dca4dfa7e1e007f4983ee44b6d2d5baab45507d2
yanagitani <takanori.yanagitani@gmail.com>
2020-05-15 14:10:09 +0900

    dict -> csv

--- csvdict.py
+++ csvdict.py
@@ -6,3 +6,8 @@ def dict2str(d=dict(), s1=":", s2=";", pairs=None): return s2.join(
     pairs or dict2kvpairs(d)
   )
 )
+
+def dict2csv(d=dict(), c=",", pairs=None): return c.join(map(
+  lambda pair: c.join(map(str, pair)),
+  pairs or dict2kvpairs(d)
+))
--- csvdict_test.py
+++ csvdict_test.py
@@ -8,6 +8,12 @@ class dict2str(unittest.TestCase):
   def test_mount(self): self.assertEqual("fuji=3776|takao=599", csvdict.dict2str(dict(fuji=3776,takao=599),"=","|"))
   pass
 
+class dict2csv(unittest.TestCase):
+  def test_empty(self): self.assertEqual("", csvdict.dict2csv(dict()))
+  def test_tower(self): self.assertEqual("tst,634,tt,333", csvdict.dict2csv(dict(tst=634, tt=333)))
+  def test_mount(self): self.assertEqual("fuji-3776-takao-599", csvdict.dict2csv(dict(fuji=3776,takao=599),"-"))
+  pass
+
 def main(): return unittest.main()
 
 def try_exec(): return "__main__" == __name__ and main()
