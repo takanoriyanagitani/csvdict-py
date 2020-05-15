27e4ed48d0d3a0b65b5fcab7f53f00d4b6d30d17
yanagitani <takanori.yanagitani@gmail.com>
2020-05-15 15:18:47 +0900

    trying to stringify nested dict...

--- csvdict.py
+++ csvdict.py
@@ -1,9 +1,9 @@
-def dict2kvpairs(d=dict()): return map(lambda k: (k, d[k]), sorted(d.keys()))
+def dict2kvpairs(d=dict(), d2k=lambda x: sorted(x.keys())): return map(lambda k: (k, d[k]), d2k(d))
 
-def dict2str(d=dict(), s1=":", s2=";", pairs=None): return s2.join(
+def dict2str(d=dict(), s1=":", s2=";", pairs=None, d2k=lambda x: sorted(x.keys())): return s2.join(
   map(
     lambda t: s1.join(map(str, t)),
-    pairs or dict2kvpairs(d)
+    pairs or dict2kvpairs(d, d2k)
   )
 )
 
@@ -11,3 +11,5 @@ def dict2csv(d=dict(), c=",", pairs=None): return c.join(map(
   lambda pair: c.join(map(str, pair)),
   pairs or dict2kvpairs(d)
 ))
+
+def ds2str(ds=None, s1=":", s2=";", d2k=lambda x: sorted(x.keys())): return dict2str(ds,s1,s2) if dict == type(ds) else ds
--- csvdict_test.py
+++ csvdict_test.py
@@ -14,6 +14,11 @@ class dict2csv(unittest.TestCase):
   def test_mount(self): self.assertEqual("fuji-3776-takao-599", csvdict.dict2csv(dict(fuji=3776,takao=599),"-"))
   pass
 
+class ds2str(unittest.TestCase):
+  def test_empty(self): self.assertEqual("", csvdict.ds2str(""))
+  def test_tower(self): self.assertEqual("tst:634;tt:333", csvdict.ds2str(dict(tst=634, tt=333)))
+  def test_mount(self): self.assertEqual("fuji=3776|takao=599", csvdict.ds2str(dict(fuji=3776,takao=599),"=","|"))
+
 def main(): return unittest.main()
 
 def try_exec(): return "__main__" == __name__ and main()
