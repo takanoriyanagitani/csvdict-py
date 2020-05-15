636a205a819bab215b93226f2499f2e9f9993bf3
yanagitani <takanori.yanagitani@gmail.com>
2020-05-15 15:31:27 +0900

    trying to convert nested dict to str...

--- csvdict.py
+++ csvdict.py
@@ -1,15 +1,23 @@
-def dict2kvpairs(d=dict(), d2k=lambda x: sorted(x.keys())): return map(lambda k: (k, d[k]), d2k(d))
+def dict2keys(d=dict()): return sorted(d.keys())
 
-def dict2str(d=dict(), s1=":", s2=";", pairs=None, d2k=lambda x: sorted(x.keys())): return s2.join(
+def dict2kvpairs(d=dict(), d2k=dict2keys): return map(lambda k: (k, d[k]), d2k(d))
+
+def dict2str(d=dict(), s1=":", s2=";", d2k=dict2keys): return s2.join(
   map(
     lambda t: s1.join(map(str, t)),
-    pairs or dict2kvpairs(d, d2k)
+    dict2kvpairs(d, d2k)
   )
 )
 
-def dict2csv(d=dict(), c=",", pairs=None): return c.join(map(
+def dict2csv(d=dict(), c=",", d2k=dict2keys): return c.join(map(
   lambda pair: c.join(map(str, pair)),
-  pairs or dict2kvpairs(d)
+  dict2kvpairs(d, d2k)
 ))
 
-def ds2str(ds=None, s1=":", s2=";", d2k=lambda x: sorted(x.keys())): return dict2str(ds,s1,s2) if dict == type(ds) else ds
+def ds2str(ds=None, s1=":", s2=";", d2k=dict2keys): return dict2str(ds,s1,s2, d2k) if dict == type(ds) else ds
+
+def dict2str2(d=dict(), s1="=|", s2=":;", d2k=dict2keys):
+  keys = d2k(d)
+  pairs = map(lambda k: (k, ds2str(d[k], s2[0], s2[1], d2k)), keys)
+  strs  = map(lambda p: s1[0].join(p), pairs)
+  return s1[1].join(strs)
