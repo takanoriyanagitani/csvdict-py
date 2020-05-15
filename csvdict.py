6be8e1c6ecb0f5a7b38e36442d4fd8fe963b7e48
yanagitani <takanori.yanagitani@gmail.com>
2020-05-15 13:53:07 +0900

    3.5,3.6: sorted must be used.

--- csvdict.py
+++ csvdict.py
@@ -1,6 +1,8 @@
-def dict2str(d=dict(), s1=":", s2=";"): return s2.join(
+def dict2kvpairs(d=dict()): return map(lambda k: (k, d[k]), sorted(d.keys()))
+
+def dict2str(d=dict(), s1=":", s2=";", pairs=None): return s2.join(
   map(
     lambda t: s1.join(map(str, t)),
-    d.items()
+    pairs or dict2kvpairs(d)
   )
 )
